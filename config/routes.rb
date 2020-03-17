8147957d15aefb7d45bfd1a5d2959dfd77716b10
Riku <joker_avan@icloud.com>
2020-03-17 17:11:08 +0900

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  resource :users, except:[:new]
 
   resources :articles
 
