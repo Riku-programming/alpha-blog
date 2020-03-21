449dd02fcf01a4dabad373efad3dfbe34472cb37
Riku <joker_avan@icloud.com>
2020-03-21 17:07:08 +0900

    Add edit update functionally and partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'signup', to: 'users#new'
-  resource :users, except:[:new]
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+
 
 end
