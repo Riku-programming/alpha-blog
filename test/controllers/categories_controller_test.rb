936d1d777333f74e70ed30e7c5a0570b76a57bdc
Riku <joker_avan@icloud.com>
2020-03-29 21:33:58 +0900

    Update test and fix error

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,21 +6,32 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
-    get "/categories"
+    get categories_path
+    # get "/categories"
     assert_response :success
   end
 
   test "should get new" do
-    get "/categories/new"
+    sign_in_as(@user, "password")
+    get new_category_path
     assert_response :success
   end
 
   test "should get show" do
-    get "/categories/(@category)"
+    get category_path(@category)
+    # get "/categories/(@category)"
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name:"sports"}}
+      assert_redirected_to categories_path
+    end
   end
 
 end
