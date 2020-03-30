9a7445dfcc58538c3d5dc9d91ab851497e59ab87
Riku <joker_avan@icloud.com>
2020-03-30 15:51:49 +0900

    Edit Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,20 @@ class CategoriesController < ApplicationController
 
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -24,7 +38,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
-
   end
 
   private
